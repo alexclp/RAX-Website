5444075d8945dd81aa189ceb060d556907d1b8ff
alexclp <alexandru.clapa@gmail.com>
2013-10-31 00:19:04 +0200

    PHP function to write in db. Has to be finished.

---
+++ preorder.php
@@ -0,0 +1,33 @@
+<?php
+function InsertData()
+{
+	$link = msql_connect();
+
+	if (!$link || !mssql_select_db('php', $link))
+	{
+		die("Unable to connect or select the database!");
+	}
+
+	$fullName = $_REQUEST['fullName'];
+	$email = $_REQUEST['email'];
+	$makeName = $_REQUEST['makeName'];
+	$carYear = $_REQUEST['carYear'];
+	$payment = $_REQUEST['payment'];
+	$city = $_REQUEST['city'];
+	$address = $_REQUEST['address'];
+	$postalCode = $_REQUEST['postalCode'];
+	$county = $_REQUEST['county'];
+	$country = $_REQUEST['country'];
+
+	$sql = ""; // Insert statement (has to be done)
+	$result = mssql_query($sql, $link);
+
+	if (!$result) 
+	{
+		echo mssql_error();
+		exit;
+	}
+	mssql_free_result($result);
+	mssql_close();
+	echo "Data successufully inserted!";
+}
