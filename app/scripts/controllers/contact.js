d718e53327eb3ecbd17d1b3ed2f2138bb718a653
Stoica Vlad <vlad.stoick@yahoo.co.uk>
2014-05-03 08:57:21 +0300

    contact form is done

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('clapaApp')
-  .controller('ContactController', function () {
-
-});
+  .controller('ContactController', function ($scope) {
+    $scope.submitForm = function (isValid) {
+      if (isValid) {
+      }
+    };
+  });
